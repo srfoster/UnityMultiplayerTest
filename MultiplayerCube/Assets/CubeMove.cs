7fcb65c7df5915a6f93ec306f5eeffc123e7468e
Adrian Lopez-Mobilia <alomo01@gmail.com>
2016-04-15 10:38:21 -0500

    initial commit

---
+++ MultiplayerCube/Assets/CubeMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CubeMove : NetworkBehaviour
+{
+    [ClientCallback]
+	void Update ()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 10);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 10);
+    }
+}
