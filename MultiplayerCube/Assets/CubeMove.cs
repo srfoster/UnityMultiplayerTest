edb872aff24637c5c03f31d2d695a0b32b779ec7
Adrian Lopez-Mobilia <alomo01@gmail.com>
2016-04-15 10:52:37 -0500

    movement fix

--- MultiplayerCube/Assets/CubeMove.cs
+++ MultiplayerCube/Assets/CubeMove.cs
@@ -6,7 +6,10 @@ public class CubeMove : NetworkBehaviour
     [ClientCallback]
 	void Update ()
     {
-        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 10);
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 10);
+        if (isLocalPlayer)
+        {
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 10);
+            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 10);
+        }
     }
 }
