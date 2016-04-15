8684d1eab61a09ef2e8b025e74bcab086a6a8ede
Adrian Lopez-Mobilia <alomo01@gmail.com>
2016-04-15 10:59:20 -0500

    rigidbody + random color

--- MultiplayerCube/Assets/CubeMove.cs
+++ MultiplayerCube/Assets/CubeMove.cs
@@ -3,13 +3,31 @@ using UnityEngine.Networking;
 
 public class CubeMove : NetworkBehaviour
 {
+    [SyncVar(hook = "SetColor")]
+    Color color;
+
+    [ServerCallback]
+    void Awake()
+    {
+        color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    void SetColor(Color c)
+    {
+        color = c;
+        GetComponent<Renderer>().material.color = color;
+    }
+
     [ClientCallback]
-	void Update ()
+	void FixedUpdate()
     {
         if (isLocalPlayer)
         {
-            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 10);
-            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 10);
+            Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 10;
+            GetComponent<Rigidbody>().velocity = velocity;
+
+            //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 10);
+            //transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 10);
         }
     }
 }
